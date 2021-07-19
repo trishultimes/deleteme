9c563853588e9b0d5c1ade0c9063aa0d3320d123
trishulnaikbannoth@gmail.com <trishulnaikbannoth@gmail.com>
2021-07-19 12:43:53 +0530

    added age variable

--- src/components/Userform.js
+++ src/components/Userform.js
@@ -1,15 +1,25 @@
 import { useState } from "react"
 
 export default function Userform(){
-    const [userform, setUserform] = useState({firstname: "Ram"})
+    const [userform, setUserform] = useState({firstname: "Ram",age:0})
     const handleEvent = function(event){
         console.log(event);
         setUserform({...userform,firstname:event.target.value})
+    }
+    const handleEventforAge = function(event){
+        console.log(event);
+        setUserform({...userform,age:event.target.value})
+    }
+    const save= function(event){
+        console.log("Username: " + userform.firstname);
+        console.log("Age: " + userform.age);
     }
     return(
         <div>
             <h3>Create User</h3>
             <input value={userform.firstname} onChange={handleEvent}></input>
+            <input value={userform.age} onChange={handleEventforAge}></input>
+            <button onClick={save}>Save</button>
         </div>
     )
 }
