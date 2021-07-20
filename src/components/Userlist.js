700be0b4e793ca7fe73dd116c430795e08406967
trishulnaikbannoth@gmail.com <trishulnaikbannoth@gmail.com>
2021-07-20 10:49:20 +0530

    day12 userlist-list

--- src/components/Userlist.js
+++ src/components/Userlist.js
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 export default function Userlist(){
     const [users, setUsers]= useState([]);
+    useEffect(function(){
+        if(users.length!=0){
+            return;
+        }
+        console.log('called........')
+        const promise = axios.get("http://localhost:4200/users")
+        promise.then(function(response){
+            setUsers(response.data);
+            console.log(response.data);
+        })
+    })
     return (
         <div>
         <table className="table table-bordered table-hover table-responsive">
@@ -12,11 +24,19 @@ export default function Userlist(){
                     <td>Age</td>
                 </tr>
             </thead>
-            <tbody>
-            <tr>
-                <td>Trishul </td>
-                <td>21</td>
-            </tr>
+        <tbody>
+        {users.map(function (user, index) {
+                        return (
+                            <tr>
+                                <td>
+                                    {user.firstname}
+                                </td>
+                                <td>
+                                    {user.age}
+                                </td>
+                            </tr>
+                        )
+                    })}
         </tbody>
         </table>
        
